09b9c61c6b7c7209042a079cf9478af9747edf34
Sebastian Landwehr <info@dword-design.de>
2021-11-14 12:53:36 +0100

    feat: init

---
+++ nuxt.config.js
@@ -0,0 +1,21 @@
+export default {
+  head: {
+    meta: [
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Simplebuy - Buy it the simple way!',
+      },
+      {
+        hid: 'og:site_name',
+        name: 'og:site_name',
+        content: 'simplebuy.io',
+      },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content: 'https://example.com',
+      },
+    ],
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    // fetch the data from somewhere
+    return { title: 'Title for product', description: 'Description for product' }
+  },
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description,
+        },
+        {
+          hid: 'og:description',
+          name: 'og:escription',
+          content: this.description,
+        },
+      ]
+    }
+  }
+}
+</script>
